42086893cdbf6af184ed6c6e355fa4c92c921ec9
yujing <medischdossier03@gmail.com>
2024-01-11 17:53:46 +0100

    menubar aangepast

--- scripts/script-enterkey2.js
+++ scripts/script-enterkey2.js
@@ -3,3 +3,4 @@ let list = document.querySelector('.menu-ul')
 if(list){
 
 }
+//als je op op de foto van hamburger klikt laat dan een extrra laag zien waar de opties kopen huren verkopen staan als details summary
