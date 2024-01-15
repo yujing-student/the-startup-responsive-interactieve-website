0614067ac57f1736810521e27fa0ac7325b35daa
yujing <medischdossier03@gmail.com>
2024-01-15 14:45:49 +0100

    testen scrolbehavor

--- v2/v2.js
+++ v2/v2.js
@@ -1,10 +1,21 @@
-ShowBooks();
-function ShowBooks () {
+ShowHamburgerMenu();
+ShowMoreText();
+function ShowHamburgerMenu () {
     let buttonicon = document.querySelector('.hamburger-icon');
     buttonicon.addEventListener('click', () => {
 
         let menu = document.querySelector('.menu-ul');
         menu.classList.toggle('menu-ul-open');
+
+    })
+}
+function ShowMoreText(){
+    let buttonFullDescription = document.querySelector('.full-description');
+    buttonFullDescription.addEventListener('click', () => {
+
+        let menu = document.querySelector('.hide-description');
+        console.log(menu);
+        menu.classList.toggle('open-description');
 
     })
 }
