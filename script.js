4618b19bce04b81b57ac631400969f6150e3fc64
yujing-local <100352887+yujing-student@users.noreply.github.com>
2024-01-15 12:41:50 +0100

    todo toegevoegd

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ function ShowBooks () {
     buttonicon.addEventListener('click', () => {
 
         let menu = document.querySelector('.menu-ul');
-        if (menu.style.display === "none") {/*if proporty is set to none*/
-            menu.style.display = "block";/*set it to blick*/
-        } else {
-            menu.style.display = "none";/*https://www.w3schools.com/js/js_function_parameters.asp*//*https://www.w3schools.com/js/js_function_definition.asp*/
-        }/*keep it to block*/
+        menu.classList.toggle('menu-ul-open');
+
     })
 }
