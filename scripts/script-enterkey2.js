88f69c6d5bad67d62380df34b2e1b7c836dcf1d1
yujing <medischdossier03@gmail.com>
2024-01-16 13:30:38 +0100

    dd issue datalist opgelost

--- scripts/script-enterkey2.js
+++
@@ -1,6 +0,0 @@
-let hamberger = document.querySelector('.hamburger');
-let list = document.querySelector('.menu-ul')
-if(list){
-
-}
-//als je op op de foto van hamburger klikt laat dan een extrra laag zien waar de opties kopen huren verkopen staan als details summary
